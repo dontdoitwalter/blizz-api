4a2f1fef941d72c8216f8c20c2c001ce8b70e8dd
dontdoitwalter <emailwaltervanhooser@gmail.com>
2018-10-10 23:45:20 -0400

    final draft

--- blizz.js
+++ blizz.js
@@ -6,6 +6,22 @@ let currentAct = 0;
 let url;
 const searchForm = document.querySelector('form');
 const section = document.querySelector('section');
+// let prelude = document.getElementById("#prelude")
+
+function preludeBtn(){
+    while(section.firstChild){
+        section.removeChild(section.firstChild);
+    }
+let article = document.createElement('article');
+let heading = document.createElement('h2');
+let text = document.createElement('p')
+heading.innerText =  "Welcome to my API Project!";
+text.innerText = "The video to your right is all of the cinematics from Diablo 3 combined into one video. In the header you can click on an Act to see what quests are in that act. Click prelude to return to this prompt. Don't worry, even though you might get a little click happy, the video will continue to play. ENJOY!"
+article.appendChild(heading);
+section.appendChild(article);
+article.appendChild(text);
+}
+
 function actOne(){
     currentAct=0;
     actInfo();
@@ -41,11 +57,11 @@ function displayResults(json) {
     }
     let quests = json.acts[currentAct].quests;
     if (quests.length === 0) {
-        console.log("No Results")
+     console.log("no result");
     } else {
         for (let i = 0; i < quests.length; i++) {
             let article = document.createElement('article');
-            let heading = document.createElement('h2');
+            let heading = document.createElement('p');
             heading.innerText = quests[i].name;
             article.appendChild(heading);
             section.appendChild(article);
